94312a303e9730bf13fb8a4d8fa747e6ab43ea45
4iwen <luky.bien@gmail.com>
2024-10-30 11:58:00 +0100

    Added all header files to main '3c.h' header file

--- include/3c/3c.h
+++ include/3c/3c.h
@@ -1,5 +1,10 @@
 #pragma once
 
-#include <3c/core.h>
-#include <3c/window.h>
-#include <3c/gfx.h>
+#include <3c/core/api.h>
+#include <3c/core/platform.h>
+#include <3c/core/assert.h>
+#include <3c/core/log.h>
+
+#include <3c/window/window.h>
+
+#include <3c/graphics/graphics.h>
